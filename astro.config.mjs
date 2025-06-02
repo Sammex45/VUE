8739b301f28d83eb5db38597142e5d65c677fa58
sammex45 <samuelemediong45@gmail.com>
2025-06-02 11:46:33 +0100

    secound commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,36 +6,105 @@ import starlight from "@astrojs/starlight";
 export default defineConfig({
   integrations: [
     starlight({
-      title: "SAM Docs",
+      title: "Vue.js",
       social: [
         {
           icon: "github",
           label: "GitHub",
-          href: "https://github.com/withastro/starlight",
+          href: "https://github.com/vuejs/",
+        },
+
+        {
+          icon: "discord",
+          label: "Discord",
+          href: "https://discord.com/invite/vue",
         },
       ],
       sidebar: [
-		{
-			label: "example",
-			items: [
-			  // Each item here is one entry in the navigation menu.
-			  { label: "example Guide", slug: "guides/example" },
-			],
-		  },
         {
-          label: "getting-started",
+          label: "Getting Started",
           items: [
-            // Each item here is one entry in the navigation menu.
-            { label: "getting-started guide", slug: "guides/getting-started" },
+            { label: "Introduction", link: "/getting-started/introduction" },
+            { label: "Quick Start", link: "/getting-started/quick-start" },
           ],
         },
-		{
-			label: "max",
-			items: [
-			  // Each item here is one entry in the navigation menu.
-			  { label: "max Guide", slug: "guides/max" },
-			],
-		  },
+
+        {
+          label: "Essentials",
+          items: [
+            { label: "Creating an Application", link: "/essentials/creating-an-application",},
+            { label: "Reactivity Fundamental", link: "/essentials/reactivity-fundamental",},
+            { label: "Template Syntax", link: "/essentials/template-syntax" },
+          ],
+        },
+
+        {
+          label: "Component In Depth",
+          items: [
+            { label: "Registration", link: "/component-in-depth/registration" },
+            { label: "Props", link: "/component-in-depth/props" },
+            { label: "Events", link: "/component-in-depth/events" },
+          ],
+        },
+
+       {
+          label: "Reusability",
+          items: [
+            { label: "Composables", link: "/reusability/composables" },
+            { label: "Custom Directives", link: "/reusability/custom-directives"},
+            { label: "Plugins", link: "/reusability/plugins" },
+          ],
+        },
+
+        {
+          label: "Built-in Component",
+          items: [
+            { label: "Transition", link: "/built-in-component/transition" },
+            { label: "Transition Group", link: "/built-in-component/transition-group"},
+            { label: "Keep Alive", link: "/reusability/keep-alive" },
+          ],
+        },
+
+        {
+          label: "Scaling Up",
+          items: [
+            { label: "Single-File Components", link: "/scaling-up/single-file-component" },
+            { label: "Routing", link: "/scaling-up/routing"},
+            { label: "State Management", link: "/scaling-up/state-management" },
+            { label: "Testing", link: "/scaling-up/testing" },
+          ],
+        },
+
+        {
+          label: "Best Practices",
+          items: [
+            { label: "Performance", link: "/best-practices/performance" },
+            { label: "Production Deployment", link: "/best-practices/production-deployment"},
+            { label: "Security", link: "/best-practices/security" },
+            
+          ],
+        },
+        
+        {
+          label: "TypeScript",
+          items: [
+            { label: "Overview", link: "/typescript/overview" },
+            { label: "TS with Composition API", link: "/typescript/ts-with-composition-api"},
+            { label: "TS with Option API", link: "/typescript/ts-with-option-api" },
+            
+          ],
+        },
+
+        {
+          label: "Extra Topics",
+          items: [
+            { label: "Ways of Using Vue", link: "/extra-topics/ways-of-using-vue" },
+            { label: "Rendering Mechanism", link: "/extra-topics/rendering-mechanism"},
+            { label: "Vue and Web Components", link: "/extra-topics/vue-and-web-components" },
+            
+          ],
+        },
+
         {
           label: "Reference",
           autogenerate: { directory: "reference" },
